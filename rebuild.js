a2e1db57ec054c516b604dbe2a2ca3308c2d6b0c
ggoodman <ggoodman@gmail.com>
2013-01-16 12:49:19 -0500

    Force rebuild on SmartOS as well as MacOS

--- rebuild.js
+++ rebuild.js
@@ -1,5 +1,5 @@
 var spawn = require('child_process').spawn;
 
-if (process.platform === 'darwin') {
+if (process.platform === 'darwin' || process.platform === 'sunOS') {
   spawn('node-gyp', ['rebuild']);
 }
